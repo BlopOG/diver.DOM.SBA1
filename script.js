b84a1ada84ab3051ce67a04ca1598f3b12f235ff
BlopOG <Ian.Gregory.Giles@gmail.com>
2026-07-19 10:35:56 -0400

    finished html and CSS and part one of java

---
+++ script.js
@@ -0,0 +1,31 @@
+const diveSitesData = {
+    ginnie: {
+        title: "Ginnie Springs",
+        type: "Freshwater Spring System",
+        depth: "Max Depth: 50 ft",
+        desc: "Crystal-clear waters with massive limestone formations and an intricate cave network popular with technical divers.",
+        img: "DOM-Ginnie.jpg",
+        },
+    grotto: {
+
+        title: "Blue Grotto",
+        type: "Freshwater Sinkhole",
+        depth: "Max Depth: 100 ft",
+        desc: "A pristine freshwater sinkhole featuring great visibility, a resident softshell turtle, and a fresh-air bell at 30 feet.",
+        img: "Blue.Grotto.png"
+    },
+    spiegel: {
+        title: "Spiegel Grove",
+        type: "Ocean Wreck",
+        depth: "Max Depth: 130 ft",
+        desc: "A massive 510-foot long vessel resting in Key Largo. A world-famous destination for advanced and wreck-certified divers.",
+        img: "Speg.Grove.png"
+    },
+    buford: {
+        title: "Buford Springs",
+        type: "Freshwater Sink / Cave",
+        depth: "Max Depth: 150 ft",
+        desc: "A deep, challenging swamp sinkhole hidden within the Chassahowitzka Wildlife Management Area, reserved for advanced wilderness diving.",
+        img: "buf.png"
+    }
+};
